c702c9af35c026be6cb4041d5cc055a555b3ecd9
Caique Pozatti <caca_pozatti@hotmail.com>
2023-06-23 16:57:33 -0300

    Project created

---
+++ src/main/java/com/Projeto/demo/Projeto23Application.java
@@ -0,0 +1,13 @@
+package com.Projeto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto23Application.class, args);
+	}
+
+}
