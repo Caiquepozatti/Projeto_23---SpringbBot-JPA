4491f52d7f20ab066f91c573d34cbf63c2baee4b
Caique Pozatti <caca_pozatti@hotmail.com>
2023-06-25 19:01:41 -0300

    Service layer, component registration

--- src/main/java/com/Projeto/demo/resources/UserResource.java
+++ src/main/java/com/Projeto/demo/resources/UserResource.java
@@ -1,11 +1,16 @@
 package com.Projeto.demo.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Projeto.demo.entities.User;
+import com.Projeto.demo.services.UserService;
 
 //Recurso Web (RESTful)que é implementado por um rest controller
 	//RESTful é uma API para criação WEB, e o RestController cria endpoints para o HTTP (são instruções do código para a WEB, e a WEB retorna)
@@ -14,12 +19,22 @@ import com.Projeto.demo.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	//GetMapping é para retornar a função public abaixo dele na WEB
 	@GetMapping
 	//ResponseEntity é um tipo especifico do spring para retornar uma resposta de requisições web
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L,"Caique Pozatti","caca@gmail.com","997417581","12345");		
+	public ResponseEntity<List<User>> findAll(){
+		List<User> u = userService.findAll();	
 		return ResponseEntity.ok().body(u);
+	}
+	
+	@GetMapping (value = "/{id}") // Pois vai receber o valor do Id para procurar
+								  // @PathVariable quer dizer que a variavel vai entrar no HTTL como comando
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User u = userService.findById(id);
+		return ResponseEntity.ok(u);
 	}
 	
 	
--- src/main/java/com/Projeto/demo/services/UserService.java
+++ src/main/java/com/Projeto/demo/services/UserService.java
@@ -1,5 +1,28 @@
 package com.Projeto.demo.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Projeto.demo.entities.User;
+import com.Projeto.demo.repositories.UserRepository;
+
+@Service //Para classificar como camada de serviço para utilizar o Autowired
 public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long Id) {
+		Optional<User> obj = userRepository.findById(Id);
+		return obj.get();
+	}
+	
 
 }
