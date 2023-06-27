84438245d82e50b6ec6e7badb79d8be741e41911
Caique Pozatti <caca_pozatti@hotmail.com>
2023-06-27 16:41:26 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/Projeto/demo/entities/Product.java
+++ src/main/java/com/Projeto/demo/entities/Product.java
@@ -4,12 +4,18 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
+
 
 @Entity
 @Table(name = "tb_product")
@@ -18,75 +24,82 @@ public class Product implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 	private	String name;
 	private String description;
 	private Double price;
-	private String imgURL;
+	private String imgUrl;
 	
-	@Transient //Para Spring boot ignorar
+	//@Transient //Para Spring boot ignorar	e não ser mapeado para o banco de dados
+	//@JsonIgnore
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {
-		
-	}	
-
-	public Product(Integer id, String name, String description, Double price, String imgURL) {
+	}
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.price = price;
-		this.imgURL = imgURL;
+		this.imgUrl = imgUrl;
 	}
-
-
-
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
-
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getName() {
 		return name;
 	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public Double getPrice() {
-		return price;
-	}
-
-	public String getImgURL() {
-		return imgURL;
-	}
-
-	public Set<Category> getCategories() {
-		return categories;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	public String getDescription() {
+		return description;
+	}
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
+	public Double getPrice() {
+		return price;
+	}
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-
-	public void setImgURL(String imgURL) {
-		this.imgURL = imgURL;
+	public String getImgUrl() {
+		return imgUrl;
 	}
-	
-	
-
-	
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	public Set<Category> getCategories() {
+		return categories;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
 }
