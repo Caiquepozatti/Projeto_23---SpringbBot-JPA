4db1cda8b2104d527a1bc0511d6d3eb6cb1ed4bb
Caique Pozatti <caca_pozatti@hotmail.com>
2023-06-30 11:10:48 -0300

    User insert

--- src/main/java/com/Projeto/demo/resources/UserResource.java
+++ src/main/java/com/Projeto/demo/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.Projeto.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.Projeto.demo.entities.User;
 import com.Projeto.demo.services.UserService;
@@ -37,6 +41,14 @@ public class UserResource {
 		return ResponseEntity.ok().body(u);
 	}
 	
+	@PostMapping //QUando for inserir algo no banco de dados
+				 //@RequestBody é para que o "User obj" seja vinculado ao corpo do HTTP (É utulizado quando usa @Post ou @Put
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = userService.insert(obj);
+		//forma adequada de inserir no banco de dados
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 	
 
 }
